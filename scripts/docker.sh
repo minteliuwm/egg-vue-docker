f3dc0f6014a872c7aefab37218976e2eb6072311
liuweimin <liuweimin@corp.netease.com>
2020-04-20 16:07:38 +0800

    build: 修改docker脚本

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -5,4 +5,4 @@ npm run build
 
 docker build -t egg-vue-docker .
 
-docker run -d --name egg-vue-docker -p 7002:7001 egg-vue-docker
+docker run --rm=true -d --name egg-vue-docker -p 7002:7001 egg-vue-docker
