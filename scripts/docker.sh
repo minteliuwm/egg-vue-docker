8872222c498cb50ae66f0fc6ec67e38eb99a204c
liuweimin <liuweimin@corp.netease.com>
2020-04-21 09:27:01 +0800

    build: docker 脚本调整

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -3,6 +3,14 @@
 npm run tsc
 npm run build
 
+# 停止服务
+docker stop egg-vue-docker
+
+# 构建镜像
 docker build -t egg-vue-docker .
 
+# 启动服务
 docker run --rm=true -d --name egg-vue-docker -p 7002:7001 egg-vue-docker
+
+# 删除 id 为 none 的镜像
+docker rmi $(docker images | grep "^<none>" | awk "{print $3}")
